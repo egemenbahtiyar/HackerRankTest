9bc9f9cc3da80b1ed91c4588819289bb170976ea
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2023-03-12 00:02:31 +0300

    Repated string was completed

--- HackerRankTest/Program.cs
+++ HackerRankTest/Program.cs
@@ -6,46 +6,39 @@ namespace HackerRankTest
 {
     class Program
     {
-        public static int jumpingOnClouds(List<int> c)
+        public static long repeatedString(string s, long n)
         {
-            int jumps = 0;
+            long aCount = 0;
+            long sLength = s.Length;
 
-
-            for (int i = 0; i < c.Count-1; i++)
+            // Count the number of "a"s in a single occurrence of the string "s".
+            for (int i = 0; i < sLength; i++)
             {
-                if (i == c.Count - 2 && c[i] == 0 && c[i + 1] == 0)
+                if (s[i] == 'a')
                 {
-                    jumps++;
+                    aCount++;
                 }
-                else if (c[i] == 0 && c[i + 1] == 0 && c[i + 2] == 0)
-                {
-                    jumps++;
-                    i+=1;
-                }
-                else if (c[i] == 0 && c[i + 1] == 0 && c[i + 2] == 1)
-                {
-                    jumps++;
-                }
-                else if (c[i] == 0 && c[i + 1] == 1 && c[i + 2] == 0)
-                {
-                    jumps++;
-                    i+=1;
-                }
-                else if (c[i] == 0 && c[i + 1] == 1 && c[i + 2] == 1)
-                {
-                    jumps+=2;
-                    i += 1;
-                }
-
             }
 
-            return jumps;
+            // Calculate the total number of "a"s in the repeated string.
+            long totalACount = aCount * (n / sLength);
+
+            // Count the remaining "a"s in the partial occurrence of "s".
+            for (int i = 0; i < n % sLength; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    totalACount++;
+                }
+            }
+
+            return totalACount;
 
 
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(jumpingOnClouds(new List<int> { 0,0,0,1,0,0 }));
+            Console.WriteLine(repeatedString("a", 1000000000000));
         }
     }
 }
